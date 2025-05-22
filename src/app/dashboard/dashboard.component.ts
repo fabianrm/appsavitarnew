4d39f7b7e65f6be5251295abd35d6832134e8f66
Fabian Ramirez <fabianrm@gmail.com>
2025-05-22 05:47:04 -0500

    Fix Ui en Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,10 +4,10 @@ import { Summary } from './Models/SummaryResponse';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrl: './dashboard.component.css',
-    standalone: false
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+  standalone: false
 })
 export class DashboardComponent implements OnInit {
 
@@ -18,6 +18,12 @@ export class DashboardComponent implements OnInit {
     { icon: 'trending_up', number: '0', label: 'Servicios Activos' },
     { icon: 'attach_money', number: '0', label: 'Planes Activos' },
     { icon: 'report_problem', number: '0', label: 'Facturas Vencidas' },
+    { icon: 'local_atm', number: '0', label: 'Pagos del día' },
+    { icon: 'payments', number: '0', label: 'Pagos del mes' },
+    { icon: 'event_busy', number: '0', label: 'Pagos Vencidos' }
+  ];
+
+  cardsTotals = [
     { icon: 'local_atm', number: '0', label: 'Pagos del día' },
     { icon: 'payments', number: '0', label: 'Pagos del mes' },
     { icon: 'event_busy', number: '0', label: 'Pagos Vencidos' }
@@ -42,14 +48,26 @@ export class DashboardComponent implements OnInit {
         { icon: 'wifi', number: this.summary.activePlans, label: 'Planes Activos' },
         { icon: 'attach_money', number: this.summary.pendingInvoices, label: 'Facturas Pendientes' },
         { icon: 'report_problem', number: this.summary.overdueInvoices, label: 'Facturas Vencidas' },
-        { icon: 'local_atm', number: this.formatCurrency(this.summary.paidDaySum), label: 'Hoy ' + this.summary.totalPaidDay + ' Pagos' },
-        { icon: 'payments', number: this.formatCurrency(this.summary.paidMonthSum), label: 'Pagos del Mes' },
-        { icon: 'event_busy', number: this.formatCurrency(this.summary.overduePaidSum), label: 'Pagos Vencidos' },
+        // { icon: 'local_atm', number: this.formatCurrency(this.summary.paidDaySum), label: 'Hoy ' + this.summary.totalPaidDay + ' Pagos' },
+        // { icon: 'payments', number: this.formatCurrency(this.summary.paidMonthSum), label: 'Pagos del Mes' },
+        // { icon: 'event_busy', number: this.formatCurrency(this.summary.overduePaidSum), label: 'Pagos Vencidos' },
       ];
+      this.getSummaryAmounts();
     });
   }
 
-  formatCurrency(value: number): string {
+
+  getSummaryAmounts() {
+
+    this.cardsTotals = [
+      { icon: 'local_atm', number: this.formatCurrency(this.summary!.paidDaySum), label: 'Hoy ' + this.summary?.totalPaidDay + ' Pagos' },
+      { icon: 'payments', number: this.formatCurrency(this.summary!.paidMonthSum), label: 'Pagos del Mes' },
+      { icon: 'event_busy', number: this.formatCurrency(this.summary!.overduePaidSum), label: 'Pagos Vencidos' },
+    ];
+
+  }
+
+  formatCurrency(value: number = 0): string {
     return new Intl.NumberFormat('es-PE', {
       style: 'currency',
       currency: 'PEN'
